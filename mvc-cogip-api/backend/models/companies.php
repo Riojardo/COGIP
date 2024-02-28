dac486be8cca502e0cefd31fe930b37b9e8d6f41
Alice Marique <mariquealice83@gmail.com>
2024-02-28 14:22:43 +0100

    .

--- mvc-cogip-api/backend/models/companies.php
+++ mvc-cogip-api/backend/models/companies.php
@@ -7,13 +7,14 @@ class Companies {
     private string $created_at;
     private string $updated_at;
 
-    public function __construct(string $name, string  $country, string  $tva, string $created_at, string $updated_at)
+    public function __construct(string $name, string  $country, string $tva, string $created_at, string $updated_at)
     {
         $this->name = $name;
         $this->country = $country;
-        $this->tva =  $tva;
+        $this->tva = $tva;
         $this->created_at = $created_at;
         $this->updated_at = $updated_at;
+      
     
     }
 }
